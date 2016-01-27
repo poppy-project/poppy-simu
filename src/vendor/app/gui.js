710cb48b912ea7ae0fdb1e316521d220397c86ad
Damien Caselli <damien.caselli@gmail.com>
2016-01-27 11:41:10 +0100

    Update texts to French

--- src/vendor/app/gui.js
+++ src/vendor/app/gui.js
@@ -24,19 +24,20 @@ define([ 'dat', 'container' ], function(dat, container) {
 
   gui.controller = {};
 
-  gui.controller.motors = gui.gui.addFolder('Motors angles');
+  gui.controller.motors = gui.gui.addFolder('Angles des moteurs');
   gui.controller.motors.add(gui.guiData, 'm1', -180, 180, 0.025).name('m1').listen();
   gui.controller.motors.add(gui.guiData, 'm2', -180, 180, 0.025).name('m2').listen();
   gui.controller.motors.add(gui.guiData, 'm3', -180, 180, 0.025).name('m3').listen();
   gui.controller.motors.add(gui.guiData, 'm4', -180, 180, 0.025).name('m4').listen();
   gui.controller.motors.add(gui.guiData, 'm5', -180, 180, 0.025).name('m5').listen();
   gui.controller.motors.add(gui.guiData, 'm6', -180, 180, 0.025).name('m6').listen();
+  gui.controller.motors.open();
 
-  gui.controller.remote = gui.gui.addFolder('Remote Control');
-  gui.controller.remote.add(gui.guiData, 'remoteStatus').name('Enable');
-  gui.controller.remoteHost = gui.controller.remote.add(gui.guiData, 'remoteHost').name('Host');
+  gui.controller.remote = gui.gui.addFolder('Contrôle à distance');
+  gui.controller.remote.add(gui.guiData, 'remoteStatus').name('Activer');
+  gui.controller.remoteHost = gui.controller.remote.add(gui.guiData, 'remoteHost').name('Hôte');
   gui.controller.remotePort = gui.controller.remote.add(gui.guiData, 'remotePort').name('Port');
-  gui.controller.remoteFrequency = gui.controller.remote.add(gui.guiData, 'remoteFrequency', 1, 35, 1).name('Frequency');
+  gui.controller.remoteFrequency = gui.controller.remote.add(gui.guiData, 'remoteFrequency', 1, 35, 1).name('Fréquence');
   gui.controller.remote.open();
 
   return gui;
