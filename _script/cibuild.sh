e6a9ff9557e848fbf97000b93a429baf49780ce5
Damien Caselli <damien.caselli@gmail.com>
2016-04-04 17:24:48 +0200

    Fix deploy script

--- _script/cibuild.sh
+++ _script/cibuild.sh
@@ -1,5 +1,6 @@
 # uncomment url in jekyll config
 sed 's/# baseurl:/baseurl:/g' '_config.yml' > _config.yml.new
+mv _config.yml.new _config.yml
 sed 's/# url:/url:/g' '_config.yml' > _config.yml.new
 mv _config.yml.new _config.yml
 
